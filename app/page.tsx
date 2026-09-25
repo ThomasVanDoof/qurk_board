5b19c58c56d2d8c9912df7bb20b564d4caf513f9
Valentina Bass Skrivanelli <aldanavalentinabass@gmail.com>
2026-09-25 17:36:20 -0300

    solving merge conflict done

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,21 @@ export default function Home() {
 
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Team Members: Marvin Canastuj, Valentina Bass, Thomas Barney, Charles Adam Davis
+                      Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
